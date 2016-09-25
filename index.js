dbda5dfcdaf0db6a96166852c4c78921eae3f0f8
Jonathan Diosa <jadiosa@gmail.com>
2016-09-24 21:59:42 -0500

    first version alarm device

--- index.js
+++ index.js
@@ -1,12 +1,123 @@
+// johnny-five - Library to control Arduino Board
 var five = require("johnny-five");
-var board = five.Board();
+var board = new five.Board();
 
-board.on("ready",function() {
-	rotary = new five.Sensor("A0");
-	var led = new five.Led(6);
+// Sensors
+var led, motion, buzzer;
 
-	rotary.scale(0, 255).on("change", function(){
-		led.brightness(this.value);
-	});
-	
+// Twilio - SMS Provider
+var twilio = require('twilio');
+var client_twilio = twilio (
+  'ACecffcadb4c0d69a1b51b00bf42341d5a', 
+  '33a778f57515d3ef01cb5c25a70d9609');
+
+// HTTP Module to Call Backend Api
+var unirest = require('unirest');
+
+// This var indicates how many times the alarm has been triggered
+var times = 0;
+// This var indicates the limit to trigger the alarm
+var limit = 2 
+
+board.on("ready", function() {
+
+  // Create a new motion hardware instance.
+  motion = new five.Motion(6);
+  // Led indicator
+  led = new five.Led(7);
+  // Buzzer
+  buzzer = new five.Piezo(4);
+  buzzer.noTone(); // No sound at beginning
+
+  // "calibrated" occurs once, at the beginning of a session,
+  motion.on("calibrated", function() {
+    console.log("calibrated");
+  });
+
+  // "motionstart" events are fired when the "calibrated"
+  // proximal area is disrupted, generally by some form of movement
+  motion.on("motionstart", function() {   
+    times++;
+    console.log("motionstart", times);
+    // Chech if alarm should be triggered
+    if(has_to_trigger()) {
+        trigger_alarm();
+    }
+  });
+
+  // "motionend" events are fired following a "motionstart" event
+  // when no movement has occurred in X ms
+  motion.on("motionend", function() {
+    console.log("motionend");
+    stop_alarm();
+  });
 });
+
+function trigger_alarm(){
+  //Reset Times Variable
+  times= 0;
+  // Led starting to blink
+  led.blink();
+  // Buzzer starting to play
+  buzzer.frequency(6000, 500);
+
+  // Json Object sended to Backend API
+  var notification = {
+    article_id: "1",
+    motion: "true"
+  };
+
+  // Sending data to Backed API
+  unirest.post('https://heyiamhere.herokuapp.com/messages')
+    .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+    .send(notification)
+    .end(function (response) {
+
+      // Json Object sended to Backend API
+      var user = {
+        phone: "+573168120372",
+        article_name: response.body.article.name
+      };
+      console.log(response.body);
+      //send_sms(user);
+  });
+}
+
+// Check if alarm should be triggered
+function has_to_trigger(){
+  if(times == limit) {
+      return true;
+  }
+  return false;
+}
+
+// Stop current active alarm
+function stop_alarm(){
+ //Led stopping to blink
+  led.stop().off();
+  // Buzzer stopping to play
+  buzzer.noTone();
+}
+
+// Send SMS Messagge
+function send_sms(user){
+  client_twilio.messages.create({
+    body: '***BiciCare*** te notifica, tu ' 
+          + user.article_name
+          + ' está en Movimiento. Ingresa de' 
+          + ' inmediato a nuestra aplicación para monitorear.',
+    to: user.phone,
+    from: '+14794312469' 
+  }, function smsResults(err, msg){
+    if (err){
+      console.log('*** Error ***\n', err);
+      return;
+    }
+    if(!msg.errorCode){
+      console.log('> Success');
+    } else {
+      console.log('> Problem: %s', msg.errorCode);
+    }
+  });
+  console.log('Sending SMS');
+}
